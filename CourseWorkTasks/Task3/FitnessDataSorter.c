329ecfa88b6e3d6019ceebc91c4b0e67df30aebe
JoshuaLim745 <146257090+JoshuaLim745@users.noreply.github.com>
2023-11-28 16:39:01 +0000

    Task 3 progress

--- CourseWorkTasks/Task3/FitnessDataSorter.c
+++ CourseWorkTasks/Task3/FitnessDataSorter.c
@@ -27,6 +27,49 @@ void tokeniseRecord(char *record, char delimiter, char *date, char *time, int *s
 
 
 int main() {
+    /* plan
+
+    structure FITNESSDATA
+
+    array of Fitnessdata with large size initially (maybe 1000)
+
+    Userinput file name
+
+    Open File for reading 
+    Check if file exist/is a proper csv file
+
+    Read line
+    (How to check for correct formatting?) --> maybe consider the type then the length of char eg date has a length of 10 char and time has length of 5 
+
+    if correct format then, use tokenize records and split on comma
+    Place it into array
+    Then Sort array using insertion sort
+
+    Then write it to a file create by concatinating the user entered name and .tsv
     
+    */
+
+   char Filename[100];
+
+   FitnessData FitnessArray[1000];
+   char date[11], time[6];
+   int steps = 0;
+
+   
+    scanf("Enter Filename: %s\n", Filename);
+
+    if(fopen(Filename,"r") == NULL){
+        printf("Error: invalid file");
+        return 1;
+
+    }
+
+    tokeniseRecord(FitnessArray, ",",date,time,steps);
     
+
+
+
+
+    fclose(Filename);
+    return 0;
 }
